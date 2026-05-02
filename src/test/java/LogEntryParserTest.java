7fbc060c24b8aea47b4620e72eb593c43bdaa788
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-05-01 21:48:40 -0500

    test: add LogEntryParser timestamp parsing test

--- src/test/java/LogEntryParserTest.java
+++ src/test/java/LogEntryParserTest.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.Test;
+import parser.LogEntryParser;
 
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LogEntryParserTest {
+class LogEntryParserTest {
+
+    @Test
+    void shouldParseDateTimeFromLog() {
+        LocalDateTime timestamp = LogEntryParser.parseDateTimeFromLog("2026-04-28","09:10:30");
+        assertEquals(LocalDateTime.of(2026, 4, 28, 9, 10, 30), timestamp);
+    }
 }
